cfc8d83613ada6ca4fac80f19aac671ecabd7682
ali taha <ali@albaap.com>
2022-02-06 17:54:08 +0300

    migrate-to-nullsafety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_map_location_picker/generated/l10n.dart'
@@ -6,13 +5,17 @@ import 'package:google_map_location_picker/generated/l10n.dart'
 import 'package:google_map_location_picker/google_map_location_picker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'generated/i18n.dart';
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
-  LocationResult _pickedLocation;
+  LocationResult? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,11 +48,14 @@ class _MyAppState extends State<MyApp> {
               children: <Widget>[
                 ElevatedButton(
                   onPressed: () async {
-                    LocationResult result = await showLocationPicker(
+                    LocationResult? result = await showLocationPicker(
                       context,
-                      apikey,
+                      "googleMapsApiKeyAndroid",
                       initialCenter: LatLng(31.1975844, 29.9598339),
+//                      automaticallyAnimateToCurrentLocation: true,
+//                      mapStylePath: 'assets/mapStyle.json',
                       myLocationButtonEnabled: true,
+                      // requiredGPS: true,
                       layersButtonEnabled: true,
                       desiredAccuracy: LocationAccuracy.best,
                       automaticallyImplyLeading: true,
