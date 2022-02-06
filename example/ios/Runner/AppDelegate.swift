4c474bd3461bf1a4dc0d2f453a9890f4bf146e3b
ali taha <ali@albaap.com>
2022-02-06 17:53:00 +0300

    migrate-to-nullsafety

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -2,12 +2,11 @@ import UIKit
 import Flutter
 import GoogleMaps
 
-
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("YOUR API KEY")
     GeneratedPluginRegistrant.register(with: self)
