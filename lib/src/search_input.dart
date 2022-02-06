59284e322de9e7dc928740edab9163e9b0b5f31f
ali taha <ali@albaap.com>
2022-02-06 18:14:07 +0300

    migrate-to-nullsafety

--- lib/src/search_input.dart
+++ lib/src/search_input.dart
@@ -1,46 +1,59 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_map_location_picker/generated/l10n.dart';
+
 /// Custom Search input field, showing the search and clear icons.
 class SearchInput extends StatefulWidget {
   SearchInput(
     this.onSearchInput, {
-    Key key,
+    Key? key,
     this.searchInputKey,
     this.boxDecoration,
     this.hintText,
   }) : super(key: key);
+
   final ValueChanged<String> onSearchInput;
-  final Key searchInputKey;
-  final BoxDecoration boxDecoration;
-  final String hintText;
+  final Key? searchInputKey;
+  final BoxDecoration? boxDecoration;
+  final String? hintText;
+
   @override
   State<StatefulWidget> createState() => SearchInputState();
 }
+
 class SearchInputState extends State<SearchInput> {
   TextEditingController editController = TextEditingController();
-  Timer debouncer;
+
+  Timer? debouncer;
+
   bool hasSearchEntry = false;
+
   @override
   void initState() {
     super.initState();
     editController.addListener(onSearchInputChange);
   }
+
   @override
   void dispose() {
     editController.removeListener(onSearchInputChange);
     editController.dispose();
+
     super.dispose();
   }
+
   void onSearchInputChange() {
     if (editController.text.isEmpty) {
       debouncer?.cancel();
       widget.onSearchInput(editController.text);
       return;
     }
+
     if (debouncer?.isActive ?? false) {
-      debouncer.cancel();
+      debouncer!.cancel();
     }
+
     debouncer = Timer(Duration(milliseconds: 500), () {
       widget.onSearchInput(editController.text);
     });
